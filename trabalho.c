fd9423c2597d4dfe83402b16f270d7fcbb7ab2f8
user <user@aceraspf5.eavb>
2019-09-27 14:13:23 -0300

    Funçao de rotacao adicionada

--- trabalho.c
+++ trabalho.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <math.h>
 
 /* Struct Vertex para implementar uma estrutura de dados de ponto */
 typedef struct vertex Vertex;
@@ -15,6 +16,7 @@ void display(void);
 void keyboard(unsigned char key, int x, int y);
 void drawPolygon(int vertex_num, ...);
 Vertex* translate(Vertex *v_array, Vertex dv, int vertex_num, ...);
+Vertex* rotate(Vertex *v_array, Vertex dv, int vertex_num, ...);
 
 int main(int argc, char **argv)
 {
@@ -42,30 +44,22 @@ void display(void)
     Vertex v_array[] = 
     {
         {.x = 0.25, .y = 0.25},
-        {.x = 0.75, .y = 0.25},
-        {.x = 0.75, .y = 0.75},
-        {.x = 0.25, .y = 0.75},
-    }, dv = {.x = 0.5, .y = 0.5}, *v_array_t = (Vertex*) malloc(4 * sizeof(Vertex)); 
-
-    
+        {.x = 0.50, .y = 0.25},
+        {.x = 0.50, .y = 0.50},
+        {.x = 0.25, .y = 0.50},
+    }, dv = {.x = 1.5, .y = 1.5}, *v_array_t = (Vertex*) malloc(4 * sizeof(Vertex)); 
+ 
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0, 0.0, 0.0);
+    glColor3f(1.0, 0.0, 0.0);
     drawPolygon(4, v_array[0], v_array[1], v_array[2], v_array[3]);                             // Desenha o polígono antes de transladar
-    v_array_t = translate(v_array_t, dv, 4, v_array[0], v_array[1], v_array[2], v_array[3]);    // Translada
-    drawPolygon(4, v_array_t[0], v_array_t[1], v_array_t[2], v_array_t[3]);                     // Desenha o polígono transladado
-    
-    /* @ Debug para ver se os pontos foram transladados */
+    //v_array_t = translate(v_array_t, dv, 4, v_array[0], v_array[1], v_array[2], v_array[3]);  // Translada
+    //v_array_t = rotate(v_array_t, dv, 4, v_array[0], v_array[1], v_array[2]);
+    //glColor3f(0.0, 0.0, 0.0);
+    //drawPolygon(4, v_array_t[0], v_array_t[1], v_array_t[2], v_array_t[3]);                   // Desenha o polígono transladado
+
+    /* @ Debug para ver se os pontos foram transformados */
     for(i = 0; i < 4; i++)
         printf("@ v_array_t[%d] = %lf\n", i, v_array_t[i]);                                     
-
-    /*
-    glBegin(GL_POLYGON);
-        glVertex2f(0.25, 0.25);
-        glVertex2f(0.75, 0.25);
-        glVertex2f(0.75, 0.75);
-        glVertex2f(0.25, 0.75);
-    glEnd();
-    */
 
     glFlush();
 }
@@ -86,7 +80,36 @@ Vertex* translate(Vertex *v_array, Vertex dv, int vertex_num, ...)
 
     return v_array;
 }
+Vertex* rotate(Vertex *v_array, Vertex dv, int vertex_num, ...)
+{
+    
+    int i;
+    va_list vertex_list;
+    Vertex mid_vertex;
 
+    va_start(vertex_list, vertex_num);
+
+    for(i = 0; i < vertex_num; i++)
+        v_array[i] = va_arg(vertex_list, Vertex);
+
+    mid_vertex.x = mid_vertex.y = (v_array[1].x + v_array[1].y) / 2; // Ponto médio da figura
+
+    for(i = 0; i < vertex_num; i++)
+    {
+        /* Translada os pontos para em torno da  origem */
+        v_array[i].x = v_array[i].x - mid_vertex.x; 
+        v_array[i].y = v_array[i].y - mid_vertex.y;
+        /* Rotaciona os pontos */
+        v_array[i].x = (v_array[i].x * cos(60 * (3.14/180))) - (v_array[i].y * sin(60 * (3.14/180)));
+        v_array[i].y = (v_array[i].y * cos(60 * (3.14/180))) + (v_array[i].x * sin(60 * (3.14/180)));
+        /* Retorna os pontos para sua posição original */
+        v_array[i].x = v_array[i].x + mid_vertex.x;
+        v_array[i].y = v_array[i].y + mid_vertex.y;
+        
+    }
+
+    return v_array;
+}
 void drawPolygon(int vertex_num, ...)
 {
     Vertex *v_array;
